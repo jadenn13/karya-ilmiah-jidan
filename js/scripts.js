917baa451756269b92bc4aefd61a3fa9e3eacacf
jadenn13 <jidanjaelani13@gmail.com>
2025-02-19 21:18:53 +0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,1 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const slides = document.querySelectorAll('.slide');
-    slides.forEach((slide, index) => {
-        if(index === 0) slide.classList.add('active');
-        slide.addEventListener('click', function() {
-            slides.forEach(s => s.classList.remove('active'));
-            this.classList.add('active');
-        });
-    });
-});
+
